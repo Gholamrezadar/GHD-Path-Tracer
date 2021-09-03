13fdd6216b5cfb793b1e256be44719415928665b
Gholamreza <rezadar13782gmail.com>
2021-09-03 19:24:49 +0430

    reafctored camera and added Multi sampling

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "utils/color.h"
 #include "utils/hittable_list.h"
 #include "primitives/sphere.h"
+#include "primitives/camera.h"
 
 #include <iostream>
 
@@ -24,6 +25,8 @@ int main()
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
+    const int samples_per_pixel = 100;
+
 
     // World
     hittable_list world;
@@ -32,19 +35,7 @@ int main()
     world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
 
     // Camera
-    auto viewport_height = 2.0;
-    auto viewport_width = aspect_ratio * viewport_height;
-    auto focal_length = 1.0;
-
-    auto origin = point3(0, 0, 0);
-    // horizontal vector. W vector
-    auto horizontal = vec3(viewport_width, 0, 0);
-    // horizontal vector. W vector
-    auto vertical = vec3(0, viewport_height, 0);
-    // Starting vector. used later to calculate every other vector on the viewport
-    // Like this : "lower_left_corner + u*horizontal + v*vertical"
-    auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
-
+    camera cam;
 
     // Render
 
@@ -58,16 +49,18 @@ int main()
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i)
         {
-            // Viewports UV coordinate
-            auto u = double(i) / (image_width-1);
-            auto v = double(j) / (image_height-1);
+            color pixel_color(0, 0, 0);
+            for (int s = 0; s < samples_per_pixel; ++s) {
+                // Screen UV coordinates
+                auto u = (i + random_double()) / (image_width-1);
+                auto v = (j + random_double()) / (image_height-1);
+                ray r = cam.get_ray(u, v);
 
-            // A ray going through the current pixel
-            ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
-            color pixel_color = ray_color(r, world);
-
-            // Write to output
-            write_color(std::cout, pixel_color);
+                // Add the color of every sample to current pixels color
+                pixel_color += ray_color(r, world);
+            }
+            // this function averages the pixel_color based on the number of samples per pixel
+            write_color(std::cout, pixel_color, samples_per_pixel);
         }
     }
 
--- src/utils/rtweekend.h
+++ src/utils/rtweekend.h
@@ -10,6 +10,8 @@
 #include <cmath>
 #include <limits>
 #include <memory>
+#include <cstdlib>
+
 
 
 // Usings
@@ -27,6 +29,22 @@ const double pi = 3.1415926535897932385;
 
 inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
+}
+
+inline double random_double() {
+    // Returns a random real in [0,1).
+    return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+    // Returns a random real in [min,max).
+    return min + (max-min)*random_double();
+}
+
+inline double clamp(double x, double min, double max) {
+    if (x < min) return min;
+    if (x > max) return max;
+    return x;
 }
 
 // Common Headers
