dc41e0b9a8e7c4549201e14cd094cf10c7c52656
Gholamreza <rezadar13782gmail.com>
2021-10-19 07:05:05 +0330

    back after a break

--- src/main.cpp
+++ src/main.cpp
@@ -35,7 +35,7 @@ int main()
 
     // Image
     const auto aspect_ratio = 16.0 / 9.0;
-    const int image_width = 888;
+    const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
     const int samples_per_pixel = 100;
 
