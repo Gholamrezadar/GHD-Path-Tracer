e980ad9a06b2df5d43ba83476548f9233289b705
Gholamreza <rezadar13782gmail.com>
2021-10-25 17:51:33 +0330

    book 1 final version

--- src/main.cpp
+++ src/main.cpp
@@ -40,11 +40,57 @@ color ray_color(const ray& r, const hittable& world, int depth) {
     return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
 }
 
+hittable_list random_scene() {
+    hittable_list world;
+
+    auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+    world.add(make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
+
+    for (int a = -11; a < 11; a++) {
+        for (int b = -11; b < 11; b++) {
+            auto choose_mat = random_double();
+            point3 center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
+
+            if ((center - point3(4, 0.2, 0)).length() > 0.9) {
+                shared_ptr<material> sphere_material;
+
+                if (choose_mat < 0.8) {
+                    // diffuse
+                    auto albedo = color::random() * color::random();
+                    sphere_material = make_shared<lambertian>(albedo);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                } else if (choose_mat < 0.95) {
+                    // metal
+                    auto albedo = color::random(0.5, 1);
+                    auto fuzz = random_double(0, 0.5);
+                    sphere_material = make_shared<metal>(albedo, fuzz);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                } else {
+                    // glass
+                    sphere_material = make_shared<dielectric>(1.5);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                }
+            }
+        }
+    }
+
+    auto material1 = make_shared<dielectric>(1.5);
+    world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+
+    auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
+    world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
+
+    auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
+    world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
+
+    return world;
+}
+
 int main()
 {
 
     // Image
-    const auto aspect_ratio = 16.0 / 9.0;
+    const auto aspect_ratio = 3.0 / 2.0;
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
     const int samples_per_pixel = 50;
@@ -52,12 +98,11 @@ int main()
 
     // World
     // W1) a plane and a sphere on top
-    
-    hittable_list world;
-    auto material_ground = make_shared<metal>(color(0.8, 0.8, 0.8));
-    auto material_ball = make_shared<lambertian>(color(0.8, 0.15, 0.05));
-    world.add(make_shared<sphere>(point3(0,0,-1), 0.5, material_ball));
-    world.add(make_shared<sphere>(point3(0,-100.5,-1), 100, material_ground));
+    // hittable_list world;
+    // auto material_ground = make_shared<metal>(color(0.8, 0.8, 0.8));
+    // auto material_ball = make_shared<lambertian>(color(0.8, 0.15, 0.05));
+    // world.add(make_shared<sphere>(point3(0,0,-1), 0.5, material_ball));
+    // world.add(make_shared<sphere>(point3(0,-100.5,-1), 100, material_ground));
 
     // W2) three spheres - one lambertian center and two metals on each side
     // hittable_list world;
@@ -71,8 +116,51 @@ int main()
     // world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
     // world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
 
+    // W3) three spheres - one metal to the right and two dielectrics on its left
+    // hittable_list world;
+    // auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
+    // auto material_center = make_shared<dielectric>(1.5);
+    // auto material_left   = make_shared<dielectric>(1.5);
+    // auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
+
+    // world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
+    // world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5, material_center));
+    // world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
+    // world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
+
+    // W3) three spheres - one metal to the right lambertian in the middle and a hollow glass on the left
+    // hittable_list world;
+    // auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
+    // auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
+    // auto material_left   = make_shared<dielectric>(1.5);
+    // auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
+
+    // world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
+    // world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5, material_center));
+    // world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),  -0.4, material_left));
+    // world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
+
+    // W4) FOV Test Scene
+    // auto R = cos(pi/4);
+    // hittable_list world;
+
+    // auto material_left  = make_shared<lambertian>(color(0,0,1));
+    // auto material_right = make_shared<lambertian>(color(1,0,0));
+
+    // world.add(make_shared<sphere>(point3(-R, 0, -1), R, material_left));
+    // world.add(make_shared<sphere>(point3( R, 0, -1), R, material_right));
+
+    //W5) Cover Scene - Random Spheres with random materials
+    auto world = random_scene();
+
     // Camera
-    camera cam;
+    point3 lookfrom(13,2,3);
+    point3 lookat(0,0,0);
+    vec3 vup(0,1,0);
+    auto dist_to_focus = 10.0;
+    auto aperture = 0.1;
+
+    camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
 
     // Render
 
@@ -83,7 +171,7 @@ int main()
     // time before rendering
     auto start_time = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
     auto end_time = start_time;
-    auto eta = 0.0;
+    float eta = 0.0;
     // Loop over pixels
     for (int j = image_height - 1; j >= 0; --j)
     {
@@ -93,9 +181,10 @@ int main()
         // time_remaining = time_elapsed *
         //                  row_remaining /
         //                  rows_elapsed
-        eta = ((end_time-start_time)/1000.0) *
-              j /
-              (image_height-j);
+        eta = static_cast<float>((end_time-start_time)) *
+              static_cast<float>(j) /
+              static_cast<float>(image_height-j) /
+              1000.0;
               
 
         std::cerr << "\rETA: " << eta << " sec " <<" | "<<(image_height-j)<<"/"<<image_height<< std::flush;
--- src/utils/material.h
+++ src/utils/material.h
@@ -36,19 +36,56 @@ class lambertian : public material {
 
 class metal : public material {
     public:
-        metal(const color& a) : albedo(a) {}
+        metal(const color& a, double f) : albedo(a), fuzz(f < 1 ? f : 1) {}
 
         virtual bool scatter(
             const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
         ) const override {
             vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
-            scattered = ray(rec.p, reflected);
+            scattered = ray(rec.p, reflected + fuzz*random_in_unit_sphere());
             attenuation = albedo;
             return (dot(scattered.direction(), rec.normal) > 0);
         }
 
     public:
         color albedo;
+        double fuzz;
+};
+
+class dielectric : public material {
+    public:
+        dielectric(double index_of_refraction) : ir(index_of_refraction) {}
+
+        virtual bool scatter(
+            const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
+        ) const override {
+            attenuation = color(1.0, 1.0, 1.0);
+            double refraction_ratio = rec.front_face ? (1.0/ir) : ir;
+
+            vec3 unit_direction = unit_vector(r_in.direction());
+            double cos_theta = fmin(dot(-unit_direction, rec.normal), 1.0);
+            double sin_theta = sqrt(1.0 - cos_theta*cos_theta);
+
+            bool cannot_refract = refraction_ratio * sin_theta > 1.0;
+            vec3 direction;
+            if (cannot_refract || reflectance(cos_theta, refraction_ratio) > random_double())
+                direction = reflect(unit_direction, rec.normal);
+            else
+                direction = refract(unit_direction, rec.normal, refraction_ratio);
+
+            scattered = ray(rec.p, direction);
+            return true;
+        }
+
+    public:
+        double ir; // Index of Refraction
+    private:
+        static double reflectance(double cosine, double ref_idx) {
+            // Use Schlick's approximation for reflectance.
+            auto r0 = (1-ref_idx) / (1+ref_idx);
+            r0 = r0*r0;
+            return r0 + (1-r0)*pow((1 - cosine),5);
+        }
 };
 
 #endif
