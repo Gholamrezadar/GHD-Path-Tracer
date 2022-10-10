09ebcba508977c72c902365235de59686aa55a52
Gholamreza Dar <rezadar1378@gmail.com>
2022-10-11 02:23:52 +0330

    clean branch

--- tools/convert_ppm_to_jpg.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for img in ../renders/*.ppm; do
-    filename=${img%.*}
-    echo ${filename/renders/renders\/converted}".jpg"
-    convert "$filename.ppm" ${filename/renders/renders\/converted}".jpg"
-done
--- tools/convert_ppm_to_png.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for img in ../renders/*.ppm; do
-    filename=${img%.*}
-    echo ${filename/renders/renders\/converted}".png"
-    convert "$filename.ppm" ${filename/renders/renders\/converted}".png"
-done
